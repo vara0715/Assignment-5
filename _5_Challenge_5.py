4993253423ce08cba433b0acf7252a1e67973d65
TandonPratham <117387692+TandonPratham@users.noreply.github.com>
2022-12-11 23:13:53 +0530

    Add files via upload

---
+++ _5_Challenge_5.py
@@ -0,0 +1,43 @@
+class Account:
+    def __init__(self, title=None, balance=0):
+        self.title = title
+        self.balance = balance
+    
+    def withdrawal(self, amount):
+        self.amount = amount
+        self.balance = self.balance - self.amount
+
+    def deposit(self, amount):
+        self.amount = amount
+        self.balance = self.balance + self.amount
+    def getBalance(self):
+        return self.balance
+
+class SavingsAccount(Account):
+    def __init__(self, title=None, balance=0, interestRate=0):
+            super().__init__(title, balance)
+            self.interestRate = interestRate
+    
+    def interestAmount(self):
+        return (self.balance * self.interestRate / 100 ) 
+        
+
+obj = SavingsAccount("Pratham",1000,5)
+print("Initial Balance : ",obj.getBalance())
+obj.withdrawal(500)
+print("Balance after withdrawal : ",obj.getBalance())
+obj.deposit(1500)
+print("After deposit : ",obj.getBalance())
+print("Intrest on current balance : ",obj.interestAmount())
+
+
+
+
+
+
+
+
+
+
+
+
